7e875888b24a96ac95e76bcaad8dc550a14afffa
sathish-s704 <sathishsathish75363@gmail.com>
2025-04-30 09:43:11 +0530

    Jenkins File Added

---
+++ src/test/java/CalculatorTest.java
@@ -0,0 +1,26 @@
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class CalculatorTest {
+    Calculator calc = new Calculator();
+
+    @Test
+    public void testAdd() {
+        assertEquals(5, calc.add(2, 3));
+    }
+
+    @Test
+    public void testSub() {
+        assertEquals(-1, calc.sub(2, 3));
+    }
+
+    @Test
+    public void testMul() {
+        assertEquals(6, calc.mul(2, 3));
+    }
+
+    @Test
+    public void testDiv() {
+        assertEquals(0, calc.div(2, 3)); // integer division
+    }
+}
